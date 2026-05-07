3d954cf62cb38f0e9ecbf1921678bfc70026d567
L1Pendz <2007rlee@gmail.com>
2026-05-07 11:00:15 +0800

    tsc

--- HospitalTriageSystem.js
+++ HospitalTriageSystem.js
@@ -319,9 +319,6 @@ function mountHospitalSystem() {
                 if (bed.getIsOccupied()) {
                     logMsg(system.dischargePatient(id));
                 }
-                else {
-                    logMsg("[INFO] Selected ".concat(id));
-                }
                 refresh();
             });
         });
